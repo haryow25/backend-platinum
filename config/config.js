7e4808b1081d895b1c90887394f17550a20e092c
Haryo Wisaksono <haryow25@gmail.com>
2022-08-26 13:50:48 +0700

    db update

--- config/config.js
+++ config/config.js
@@ -4,24 +4,24 @@ if (process.env.NODE_ENV === "development") require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "postgres",
-    password: "jancuk25",
-    database: "database_be-3",
-    host: "127.0.0.1",
+    username: "xvcfsiawvxhbvw",
+    password: "ca18f876a588fecebb085588724a4d4b05fc827953b8d2fafdb91860899a6bba",
+    database: "dcg524c9or0897",
+    host: "ec2-3-223-242-224.compute-1.amazonaws.com",
     dialect: "postgres",
   },
   test: {
-    username: "postgres",
-    password: "jancuk25",
-    database: "database_be-3",
-    host: "127.0.0.1",
+    username: "xvcfsiawvxhbvw",
+    password: "ca18f876a588fecebb085588724a4d4b05fc827953b8d2fafdb91860899a6bba",
+    database: "dcg524c9or0897",
+    host: "ec2-3-223-242-224.compute-1.amazonaws.com",
     dialect: "postgres",
   },
   production: {
-    username: "postgres",
-    password: "jancuk25",
-    database: "database_be-3",
-    host: "127.0.0.1",
+    username: "xvcfsiawvxhbvw",
+    password: "ca18f876a588fecebb085588724a4d4b05fc827953b8d2fafdb91860899a6bba",
+    database: "dcg524c9or0897",
+    host: "ec2-3-223-242-224.compute-1.amazonaws.com",
     dialect: "postgres",
   },
 };
