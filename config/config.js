836c6adbba020c9b0350301c92efce8daa70a4da
Haryo Wisaksono <haryow25@gmail.com>
2022-08-26 10:46:07 +0700

    first commit

--- config/config.js
+++ config/config.js
@@ -1,27 +1,27 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 const upperCaseEnv = env.toUpperCase();
-if (process.env.NODE_ENV === 'development') require('dotenv').config();
+if (process.env.NODE_ENV === "development") require("dotenv").config();
 
 module.exports = {
   development: {
-    "username": "postgres",
-    "password": "123456",
-    "database": "database_10ch",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    username: "postgres",
+    password: "jancuk25",
+    database: "database_be-3",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
   test: {
-    "username": "postgres",
-    "password": "123456",
-    "database": "database_10ch",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    username: "postgres",
+    password: "jancuk25",
+    database: "database_be-3",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
   production: {
-    "username": "postgres",
-    "password": "123456",
-    "database": "database_10ch",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    username: "postgres",
+    password: "jancuk25",
+    database: "database_be-3",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
 };
